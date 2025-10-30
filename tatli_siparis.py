ed7a9f44b9355158a47cdc6bf7deac073365d4c3
Umutcan Durbak <umutcan_durbak@outlook.com>
2025-10-30 16:54:28 +0300

    Update version

--- tatli_siparis.py
+++ tatli_siparis.py
@@ -22,7 +22,7 @@ except ImportError:
 
 # PyInstaller ile build ederken .ico dosyasını eklemeyi unutmayın!
 ICON_PATH = "appicon.ico"
-VERSION = "v1.2.5"
+VERSION = "v1.2.6"
 DEVELOPER = "Developer U.D"
 
 # Güncelleme ayarları
